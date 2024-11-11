18e40547221a79964ebe1ccb6f41b09abdd29fd8
Alan934 <sanjurjoalan77@gmail.com>
2024-11-11 14:26:41 -0300

    restore to build correct

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,16 @@
 import dotenv from "dotenv";
 import { DatabaseType } from "typeorm";
 import * as Entities from "./entities";  // Asegúrate de importar todas las entidades correctamente
-import { Flow } from "./entities/flow/flow.model";
 
 dotenv.config();
 
 export const dbConfig = {
   type: (process.env.DB_TYPE as DatabaseType) || "postgres",
   url: process.env.DB_URL || "sqlite:memory",
-  entities: Object.values(Entities, Flow),
+  entities: Object.values(Entities),
   synchronize: true, 
   subscribers: [],
   migrations: [],
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
 };
 
 export const limit = 20;  // Límite de items devueltos por página
