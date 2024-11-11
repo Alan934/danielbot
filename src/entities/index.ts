27408f7d87d782e106f19c0f80bc0c903c2639c4
Alan934 <81592583+Alan934@users.noreply.github.com>
2024-11-11 17:27:00 -0300

    Update index.ts

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,7 +1,7 @@
 
 export * from "./base/base.model";
 export * from "./agent/profile.model";
-export * from "./client";
+export * from "./client/client.model";
 export * from "./enterprise/enterprise.model";
 export * from "./message/message.model";
 export * from "./flow/flow.model";
