3eb1c7bff078d1d66707ab25285f91d5ee06bc63
Alan934 <81592583+Alan934@users.noreply.github.com>
2024-11-14 02:05:52 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { AppDataSource } from "./data-source";
 import * as Routes from "./routes/index";
-import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./docs/swagger-output.json";
+//import { SwaggerUiOptions } from "./docs/swaggerUiOptions";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -21,7 +22,6 @@ apiV1Router.use("/enterprises", Routes.enterpriseRouter());
 apiV1Router.use("/profiles", Routes.profileRouter());
 apiV1Router.use("/clients", Routes.clientRouter());
 apiV1Router.use("/messages", Routes.messageRouter());
-apiV1Router.use("/submessages", Routes.subMessageRouter());
 apiV1Router.use("/flows", Routes.flowRouter());
 apiV1Router.use("/plans", Routes.pricingPlanRouter());
 apiV1Router.use("/authenticated", Routes.authenticatedRoute());
@@ -30,21 +30,37 @@ apiV1Router.use("/session", Routes.sessionRouter());
 app.use("/docs", swaggerUi.serve);
 
 //Utilizar este en desarrollo
-app.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+//app.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 //Utilizar este en produccion
 
-// app.get("/docs", swaggerUi.setup(swaggerFile, {customSiteTitle: 'Backend Generator',
-// customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
-// customJs: [
-//   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-//   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-// ],
-// customCssUrl: [
-//   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-//   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-//   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-// ],}));
+// const swaggerConfig: SwaggerUiOptions = {
+//   customSiteTitle: 'Backend Generator',
+//   customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
+//   customJs: [
+//     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+//     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+//   ],
+//   customCssUrl: [
+//     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+//     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+//     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+//   ],
+// };
+
+// app.get("/docs", swaggerUi.setup(swaggerFile, swaggerConfig));
+
+app.get("/docs", swaggerUi.setup(swaggerFile, {customSiteTitle: 'Backend Generator',
+customfavIcon: 'https://avatars.githubusercontent.com/u/185267919?s=400&u=7d74f9c123b27391d3f11da2815de1e9a1031ca9&v=4',
+customJs: [
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+],
+customCssUrl: [
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+],}));
 
 // Middleware para manejar rutas no encontradas
 app.use((_req: Request, res: Response) => {
